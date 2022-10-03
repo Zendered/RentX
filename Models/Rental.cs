657f353389d532a1fecabc57c9012f76148e31c3
Luiz <luizz.ggr@gmail.com>
2022-10-03 17:01:39 -0300

    rental model

---
+++ Models/Rental.cs
@@ -0,0 +1,15 @@
+﻿namespace RentX.Models
+{
+    public class Rental
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid CarId { get; set; } = Guid.NewGuid();
+        public Guid UserId { get; set; } = Guid.NewGuid();
+        public DateTime Start_Date { get; set; }
+        public DateTime End_Date { get; set; }
+        public DateTime Expected_Return_Date { get; set; }
+        public int Total { get; set; }
+        public DateTime Updated_At { get; set; } = DateTime.Now;
+        public DateTime Created_At { get; set; } = DateTime.Now;
+    }
+}
